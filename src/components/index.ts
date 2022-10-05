4b0e98dcc4df16b18a937d117948d2fc6ed01888
zyfhup <3375926016@qq.com>
2022-10-05 08:47:48 +0800

    添加了几个新文件

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+// import { Tabbar } from "@/components/tabbar";
+
+import type { App } from "vue";
+
+export default {
+    install(app:App<Element>){
+        // app.component('Tabbar',Tabbar);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-import "./assets/lib/lib-flexable.js"
-
+import "./assets/lib/lib-flexable.js";
+import  GlobalComponents  from "@/components";
 const app = createApp(App);
+
+app.use(GlobalComponents)
 
 app.use(createPinia());
 app.use(router);
