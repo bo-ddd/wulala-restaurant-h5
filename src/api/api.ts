0637bde8db850f20d7556ac0d816286e1a78d023
zyfhup <3375926016@qq.com>
2022-11-05 08:42:08 +0800

    修改了头像不上传的问题

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,7 +10,7 @@ const instance = axios.create({
 });
 const upload = axios.create({
     baseURL: '/api',
-    timeout: 1000,  //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
+    timeout: 5000,  //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
     // headers: { 'token': token },  //在发送服务端之前，前端设置请求头信息；
     headers: { 'Content-Type': 'multipart/form-data' },
 });
